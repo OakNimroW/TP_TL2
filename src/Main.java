e9752e600b849ae32bcc70e2ec0d372ada5db610
OakNimroW <lorenzomajoros@gmail.com>
2023-12-11 17:37:33 -0300

    Ordenadores por vida y alfabeticamente. Funcion para utilizar ordenador en el Main

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import game.components.RumbleGame;
 import game.monsters.EvilBeast;
 import game.monsters.IceBeast;
 import game.monsters.Spartan;
+import game.types.Type;
 
 import java.util.Arrays;
 import java.util.List;
 
 import entregable.monstruos.SwordsMan;
 import entregable.monstruos.Tank;
+import entregable.ordenadores.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,35 +21,42 @@ public class Main {
                 new Spartan("Spartan 2"),
                 new Spartan("Spartan 3"),
                 new Spartan("Spartan 4"),
-                new SwordsMan("Samurai 1",200, 3, 8),
                 new Spartan("Spartan 5"),
+                //new SwordsMan("Samurai 1",200, 3, 8),
                 new Spartan("Spartan 6"),
-                new Spartan("Spartan 7"),
-                new Spartan("Spartan 8"),
+                new SwordsMan("Samurai 3"),
                 new Spartan("Spartan 9"),
                 new Spartan("Spartan 10"),
                 new IceBeast("Ice Beast"),
                 new Spartan("Spartan 24"),
-                new Spartan("Spartan 224"),
-                new Spartan("Spartan 34"),
-                new Spartan("Spartan 44"),
+                new SwordsMan("Samurai 2"),
                 new Spartan("Spartan 64"),
                 new Spartan("Spartan 15"));
 
         //TODO ordenar el listado de monstruos que recibe el jugador uno
+        monstersOne = ordenarListaMonstruo(monstersOne, new OrdenadorPreferenciaDeTipo(Arrays.asList(Type.ANTITANK, Type.FIGHTER)));
+
         rumbleGame.getPlayerOne().setMonsters(monstersOne);
 
         List<Monster> monstersTwo = Arrays.asList(new Spartan("Spartan A"),
                 new Spartan("Spartan B"),
-                new Tank("Tank A"),
                 new Tank("Tank B"),
                 new SwordsMan("Samurai A", 200, 3, 8),
                 new SwordsMan("Samurai B"),
+                new Tank("Tank A"),
                 new SwordsMan("Samurai C", 300, 1, 5));
 
         //TODO ordenar el listado de monstruos que recibe el jugador dos
+
+        monstersTwo = ordenarListaMonstruo(monstersTwo, new OrdenadorVidaInicioMayor());
+
         rumbleGame.getPlayerTwo().setMonsters(monstersTwo);
 
         rumbleGame.startGame();
+    }
+
+    private static List<Monster> ordenarListaMonstruo(List<Monster> lista, Ordenador orden) {
+        return orden.ordenar(lista);
+
     }
 }
