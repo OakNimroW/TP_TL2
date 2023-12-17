1e72ff5092c9db63a583ea68207483092d031110
Tomás Badenes <tomasbadenes@gmail.com>
2023-12-17 19:09:57 -0300

    docs for juan's stuff

--- src/game/engine/Animation.java
+++ src/game/engine/Animation.java
@@ -2,22 +2,42 @@ package game.engine;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * La clase Animation representa una animación que puede reproducir una
+ * secuencia de imágenes en un bucle o una sola vez.
+ */
 public class Animation {
+  /**
+   * Enumeración que define las direcciones de reproducción de la animación.
+   */
   public enum Direction {
     FORWARD_ONCE,
     BACKWARD_ONCE,
     FORWARD_LOOP,
     BACKWARD_LOOP;
 
+    /**
+     * Verifica si la dirección de reproducción es hacia adelante.
+     * 
+     * @return true si la dirección es hacia adelante, false de lo contrario.
+     */
     public boolean isForward() {
       return this == FORWARD_ONCE || this == FORWARD_LOOP;
     }
 
+    /**
+     * Verifica si la animación se reproduce en bucle.
+     * 
+     * @return true si la animación se reproduce en bucle, false de lo contrario.
+     */
     public boolean isLoop() {
       return this == FORWARD_LOOP || this == BACKWARD_LOOP;
     }
   };
 
+  /**
+   * Enumeración que define los estados de la animación.
+   */
   public enum Status {
     PLAYING,
     PAUSED,
@@ -31,13 +51,31 @@ public class Animation {
   private Direction direction;
   private Status status;
 
+  /**
+   * Crea una nueva instancia de Animation con los fotogramas y la duración de
+   * fotograma especificados.
+   * La dirección de reproducción predeterminada es hacia adelante una sola vez.
+   * 
+   * @param frames        los fotogramas de la animación.
+   * @param frameDuration la duración de cada fotograma en ticks.
+   * @throws IllegalArgumentException si el arreglo de fotogramas está vacío.
+   */
   public Animation(BufferedImage[] frames, int frameDuration) {
     this(frames, frameDuration, Direction.FORWARD_ONCE);
   }
 
+  /**
+   * Crea una nueva instancia de Animation con los fotogramas, la duración de
+   * fotograma y la dirección de reproducción especificados.
+   * 
+   * @param frames             los fotogramas de la animación.
+   * @param frameDuration      la duración de cada fotograma en ticks.
+   * @param animationDirection la dirección de reproducción de la animación.
+   * @throws IllegalArgumentException si el arreglo de fotogramas está vacío.
+   */
   public Animation(BufferedImage[] frames, int frameDuration, Direction animationDirection) {
     if (frames.length == 0) {
-      throw new IllegalArgumentException("Frames array must not be empty");
+      throw new IllegalArgumentException("El arreglo de fotogramas no debe estar vacío");
     }
     this.frames = frames;
     this.currentFrame = 0;
@@ -47,27 +85,50 @@ public class Animation {
     this.status = Status.PLAYING;
   }
 
+  /**
+   * Inicia la reproducción de la animación desde el fotograma actual.
+   * Si el fotograma actual es mayor o igual que la cantidad de fotogramas, no se
+   * realiza ninguna acción.
+   */
   public void start() {
     if (currentFrame < frames.length) {
       status = Status.PLAYING;
     }
   }
 
+  /**
+   * Pausa la reproducción de la animación si está en estado de reproducción.
+   */
   public void pause() {
     if (status == Status.PLAYING) {
       status = Status.PAUSED;
     }
   }
 
+  /**
+   * Detiene la reproducción de la animación.
+   */
   public void stop() {
     status = Status.STOPPED;
   }
 
+  /**
+   * Reinicia la animación al fotograma inicial y reinicia el contador de ticks.
+   */
   public void restart() {
     currentFrame = 0;
     ticksCount = 0;
   }
 
+  /**
+   * Obtiene el fotograma actual de la animación.
+   * Si la dirección de reproducción es hacia adelante, se devuelve el fotograma
+   * actual.
+   * Si la dirección de reproducción es hacia atrás, se devuelve el fotograma en
+   * sentido inverso.
+   * 
+   * @return el fotograma actual de la animación.
+   */
   public BufferedImage getSprite() {
     if (direction.isForward()) {
       return frames[currentFrame];
@@ -76,10 +137,26 @@ public class Animation {
     }
   }
 
+  /**
+   * Obtiene el estado actual de la animación.
+   * 
+   * @return el estado actual de la animación.
+   */
   public Status getStatus() {
     return status;
   }
 
+  /**
+   * Actualiza la animación avanzando al siguiente fotograma si corresponde.
+   * Si la animación está en estado de reproducción, se incrementa el contador de
+   * ticks.
+   * Cuando el contador de ticks alcanza la duración de fotograma, se avanza al
+   * siguiente fotograma.
+   * Si la animación llega al final de los fotogramas y la dirección de
+   * reproducción es en bucle, se reinicia al primer fotograma.
+   * Si la animación llega al final de los fotogramas y la dirección de
+   * reproducción no es en bucle, se detiene la reproducción.
+   */
   public void tick() {
     if (status == Status.PLAYING) {
       ticksCount++;
--- src/game/engine/SpriteSheet.java
+++ src/game/engine/SpriteSheet.java
@@ -6,10 +6,19 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * La clase SpriteSheet representa una hoja de sprites que contiene múltiples imágenes de sprites.
+ * Cada imagen de sprite en la hoja de sprites se puede acceder mediante su posición en la cuadrícula.
+ */
 public class SpriteSheet {
   private BufferedImage spriteSheet;
   private static final int TILE_SIZE = 16;
 
+  /**
+   * Crea una nueva instancia de SpriteSheet con el nombre especificado.
+   *
+   * @param name el nombre de la hoja de sprites
+   */
   public SpriteSheet(String name) {
     try {
       spriteSheet = ImageIO.read(new File("assets/sprite_sheet_" + name + "_16x16.png"));
@@ -18,6 +27,12 @@ public class SpriteSheet {
     }
   }
 
+  /**
+   * Obtiene una fila de imágenes de sprite de la hoja de sprites en la posición de la cuadrícula especificada.
+   *
+   * @param yGrid la posición de la cuadrícula en el eje Y
+   * @return un arreglo de BufferedImage que representa la fila de imágenes de sprite
+   */
   private BufferedImage[] getRowFromSpriteSheet(int yGrid) {
     BufferedImage[] row = new BufferedImage[spriteSheet.getWidth() / TILE_SIZE];
     for (int i = 0; i < row.length; i++) {
@@ -26,26 +41,56 @@ public class SpriteSheet {
     return row;
   }
 
+  /**
+   * Obtiene la animación de inactividad.
+   *
+   * @return la animación de inactividad
+   */
   public Animation getIdleAnimation() {
     return new Animation(getRowFromSpriteSheet(0), 8, Animation.Direction.FORWARD_LOOP);
   }
 
+  /**
+   * Obtiene la animación de caminar.
+   *
+   * @return la animación de caminar
+   */
   public Animation getWalkAnimation() {
     return new Animation(getRowFromSpriteSheet(1), 8, Animation.Direction.FORWARD_LOOP);
   }
 
+  /**
+   * Obtiene la animación de ataque.
+   *
+   * @return la animación de ataque
+   */
   public Animation getAttackAnimation() {
     return new Animation(getRowFromSpriteSheet(2), 8, Animation.Direction.FORWARD_ONCE);
   }
 
+  /**
+   * Obtiene la animación de recibir daño.
+   *
+   * @return la animación de recibir daño
+   */
   public Animation getDamageTakenAnimation() {
     return new Animation(getRowFromSpriteSheet(3), 8, Animation.Direction.FORWARD_ONCE);
   }
 
+  /**
+   * Obtiene la animación de muerte.
+   *
+   * @return la animación de muerte
+   */
   public Animation getDeathAnimation() {
     return new Animation(getRowFromSpriteSheet(4), 8, Animation.Direction.FORWARD_ONCE);
   }
 
+  /**
+   * Obtiene una animación alternativa.
+   *
+   * @return la animación alternativa
+   */
   public Animation getAlternativeAnimation() {
     return new Animation(getRowFromSpriteSheet(5), 8, Animation.Direction.FORWARD_ONCE);
   }
