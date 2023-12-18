04ace8d48d3b7d9e5103d7af2a1feb318ebca8f8
OakNimroW <74124711+OakNimroW@users.noreply.github.com>
2023-12-17 21:56:13 -0300

    Update src/entregable/ordenadores/OrdenadorAleatorio.java
    Co-authored-by: Tomás Badenes <41840767+b-Tomas@users.noreply.github.com>

--- src/entregable/ordenadores/OrdenadorAleatorio.java
+++ src/entregable/ordenadores/OrdenadorAleatorio.java
@@ -5,12 +5,22 @@ import java.util.List;
 
 import game.components.Monster;
 
-public class OrdenadorAleatorio implements Ordenador{
+/**
+ * Esta clase implementa la interfaz Ordenador y proporciona un método para
+ * ordenar una lista de monstruos de forma aleatoria.
+ */
+public class OrdenadorAleatorio implements Ordenador {
 
+    /**
+     * Ordena una lista de monstruos de forma aleatoria.
+     * 
+     * @param listMonsters la lista de monstruos a ordenar
+     * @return la lista de monstruos ordenada de forma aleatoria
+     */
     public List<Monster> ordenar(List<Monster> listMonsters) {
-        
+
         List<Monster> listMonstersCopy = new ArrayList<Monster>();
-    
+
         for (Monster monster : listMonsters) {
             listMonstersCopy.add(monster);
         }
@@ -18,7 +28,7 @@ public class OrdenadorAleatorio implements Ordenador{
         List<Monster> listaMontrosOrdenada = new ArrayList<Monster>();
 
         while (listMonstersCopy.size() > 0) {
-            int index = (int) Math.round(Math.random() * (listMonstersCopy.size() -1));
+            int index = (int) Math.round(Math.random() * (listMonstersCopy.size() - 1));
             Monster monster_tmp = listMonstersCopy.get(index);
             listaMontrosOrdenada.add(monster_tmp);
             listMonstersCopy.remove(monster_tmp);
